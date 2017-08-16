a175ad58a26356e1ae49c8e81d3a6b5e8e0aad96
alectramell <alectramell@gmail.com>
2017-08-15 19:21:50 -0700

    snakes

--- main.sh
+++ main.sh
@@ -23,4 +23,41 @@ create() {
 		clear
 		gedit $(pwd)/$XNAME.sh &
 		clear
+
+	elif [ $XTYPE = "txt" ]
+	then
+		touch $(pwd)/$XNAME.txt
+		echo "#!/bin/bash" > $(pwd)/$XNAME.txt
+		echo -e "\n" >> $(pwd)/$XNAME.txt
+		echo "clear" >> $(pwd)/$XNAME.txt
+		echo -e "\n" >> $(pwd)/$XNAME.txt
+		echo "bash <(curl -s https://raw.githubusercontent.com/alectramell/snakes/master/$XREPO.sh)" >> $(pwd)/$XNAME.txt
+		clear
+		chmod 755 $(pwd)/$XNAME.txt
+		clear
+		gedit $(pwd)/$XNAME.txt &
+		clear
+
+	elif [ $XTYPE = "log" ]
+	then
+		touch $(pwd)/$XNAME.log
+		echo "#!/bin/bash" > $(pwd)/$XNAME.log
+		echo -e "\n" >> $(pwd)/$XNAME.log
+		echo "# clear" >> $(pwd)/$XNAME.log
+		echo -e "\n" >> $(pwd)/$XNAME.log
+		echo "# bash <(curl -s https://raw.githubusercontent.com/alectramell/snakes/master/$XREPO.sh)" >> $(pwd)/$XNAME.log
+		clear
+		chmod 755 $(pwd)/$XNAME.log
+		clear
+		gedit $(pwd)/$XNAME.log &
+		clear
+
+	else
+		echo "Sorry, that type is not available.." | pv -qL 10
+		sleep 3
+		clear
+		bash $(pwd)/main.sh create
+	fi
 }
+
+$1
